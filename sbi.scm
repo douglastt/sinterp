bf1cabf07d8e347ad8ea14fd0e3ba27d1586e675
Jian Liu <874628740@qq.com>
2020-01-18 16:30:10 +0800

    update

--- sbi.scm
+++ sbi.scm
@@ -48,13 +48,148 @@
          (when (not (eq? token eof)) (dump-stdin))))
 
 
+;; interp from here
+
+(define NAN (/ 0.0 0.0))
+
+(define *function-table* (make-hash))
+(for-each
+    (lambda (symfun) (hash-set! *function-table* (car symfun) (cadr symfun)))
+    `(
+        (+    ,+)
+        (-    ,-)
+        (*    ,*)
+        (/    ,/)
+        (^    ,expt)
+        (sqrt ,sqrt)
+	(=    ,=)
+	(!=   ,(lambda (a1 a2) (not (= a1 a2))))
+	(<    ,<)
+	(>    ,>)
+	(<=   ,<=)
+	(>=   ,>=)
+    ))
+
+(define *variable-table* (make-hash))
+(for-each
+    (lambda (varval)
+        (hash-set! *variable-table* (car varval) (cadr varval)))
+    `(
+        (pi    ,(acos -1))
+        (e     ,(exp 1))
+        (i     ,(sqrt -1))
+        (one   1)
+        (zero  0)
+	(eof   0.0)
+	(nan   ,(/ 0.0 0.0))
+    ))
+
+(define *array-table* (make-hash))
+(define *label-table* (make-hash))
+
+(define (dump-tables)
+  (printf "*function-table*:\n")
+  (hash-for-each
+   *function-table*
+   (lambda (key value) (printf "~s: ~s\n" key value) #t))
+  (printf "*variable-table*:\n")
+  (hash-for-each
+   *variable-table*
+   (lambda (key value) (printf "~s: ~s\n" key value) #t))
+  (printf "*array-table*:\n")
+  (hash-for-each
+   *array-table*
+   (lambda (key value) (printf "~s: ~s\n" key value) #t))
+  (printf "*label-table*:\n")
+  (hash-for-each
+   *label-table*
+   (lambda (key value) (printf "~s: ~s\n" key value) #t))
+  (printf "dump tables finished\n"))
+
+(define (remove-lino program)
+;  (let (line (car program))
+ ;   (if (empty? line)
+	
+  (map (lambda (line)
+	 (if (empty? (cdr line))
+	     (cdr line)
+	     (cadr line)))
+       program))
+;  (printf "removing lino: ~v\n" program)
+;  (if (empty? program)
+;      '()
+;      `(,(cdr (car program)) ,(remove-lino (cdr program)))
+;      ))
+
+;(define (eval-expr expr)
+;    (cond ((number? expr) (+ expr 0.0))
+;          ((symbol? expr) (hash-ref *variables* expr NAN))
+;          ((pair? expr) 
+;              (let ((func (hash-ref *functions* (car expr) NAN))
+;                    (opnds (map eval-expr (cdr expr))))
+;                   (if (null? func) NAN
+;                       (apply func (map eval-expr opnds)))))
+;           (else NAN)))
+
+
+(define (interpret-expression expr)
+  (cond ((number? expr) (+ expr 0.0))
+	((boolean? expr) expr)
+	((string? expr) expr)
+	((symbol? expr) (hash-ref *variable-table* expr NAN))
+	((pair? expr)
+	 (let ((operator (hash-ref *function-table* (car expr) NAN))
+	       (operands (map interpret-expression (cdr expr))))
+	   (if (null? operator)
+	       NAN
+	       (apply operator operands))))
+	(else NAN)))
+
+(define (print-exprs exprs)
+  (if (empty? exprs)
+      (printf "\n")
+      (begin
+	(printf "~a" (car exprs))
+	(print-exprs (cdr exprs)))))
+
+(define (interpret-statement stmt)
+  (cond ((null? stmt) '())
+	(else
+	 (let ((header (car stmt)))
+	  (cond ((eqv? header 'goto) (cdr stmt))
+		((eqv? header 'print) (print-exprs (map interpret-expression (cdr stmt))))
+		(else (raise  header)))
+	))))
+
+(define (interpret-program program)
+;  (printf "interpret-program at ~a...\n" (car program))
+  (if (empty? program)
+      (void)	; (dump-tables)
+      (let ((stmt (car program)) 
+	     (cont (cdr program)))
+	(begin
+	  ;(if (is-label-stmt stmt)
+	  ;    (put-label stmt)
+	  ;    (empty-statement))
+	  (interpret-statement stmt)
+	;  (printf "interpret a statement success\n")
+	  (interpret-program cont)))))
+
+
+;; interp end here
+
 (define (write-program-by-line filename program)
     (printf "==================================================~n")
     (printf "~a: ~a~n" *run-file* filename)
     (printf "==================================================~n")
     (printf "(~n")
     (for-each (lambda (line) (printf "~a~n" line)) program)
-    (printf ")~n"))
+;    (interpret-program (remove-lino program))
+    (printf ")~n")
+    (let ((program-nolino (remove-lino program)))
+      (for-each (lambda (line) (printf "~a~n" line)) program-nolino)
+      (interpret-program program-nolino))
+    )
 
 (define (main arglist)
     (if (or (null? arglist) (not (null? (cdr arglist))))
